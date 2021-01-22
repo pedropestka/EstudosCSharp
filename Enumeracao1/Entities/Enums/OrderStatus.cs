b137b82c967f6fa1d27ef5afeda61ce9bcf6a481
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-22 07:35:21 -0300

    Enumeracao 1 NA S09 aula115

---
+++ Enumeracao1/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace Enumeracao1.Entities.Enums
+{
+    enum OrderStatus : int // Derivaçao de tipos >> orderstatus é derivado do tipo int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
+
+/*
+ pode ter ou nao o numero:
+ PendingPayment,
+*/
+
---
+++ Enumeracao1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Enumeracao1.Entities;
+using Enumeracao1.Entities.Enums;
+using System;
+
+namespace Enumeracao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment //PendingPayment é uma opção possível
+            };
+
+            Console.WriteLine(order);
+
+            // conversar de string para enumeraçao
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); // conversao str para tipo enum
+            // parametrizacao como se fosse lista
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+
+            Console.ReadLine();
+        }
+    }
+}
+/*
+
+Conversões
+Para o tipo original: use casting
+OrderStatus os1 = (OrderStatus)2;
+int n1 = (int)OrderStatus.Processing;
+
+###string - enum:
+
+string txt = OrderStatus.PendingPayment.ToString();
+
+OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+Console.WriteLine(os);
+Console.WriteLine(txt);
+
+*/
