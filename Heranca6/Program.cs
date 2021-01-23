522bb7ee7b7b709abd44534633d112c306dc5f5b
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-23 19:28:34 -0300

    Heranca 6 Classes Abstratas NA S09 aula 134

--- Heranca6/Program.cs
+++ Heranca6/Program.cs
@@ -41,14 +41,11 @@ namespace Heranca6
 }
 
 
-
 /*
  * CLASSES ABSTRATAS
  * 
  * São classes que não podem ser instanciadas
  * É uma forma de garantir herança total: somente subclasses não
  * abstratas podem ser instanciadas, mas nunca a superclasse abstrata
- * 
- * 
- * 
+ *  
  */
