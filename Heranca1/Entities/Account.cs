e7cdb43a3e71e20366d96191b0fb8fc26ae0bf9e
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-23 12:12:52 -0300

    Heranca 1 Upcasting Downcasting Base NA S09 aula126 e 127

---
+++ Heranca1/Entities/Account.cs
@@ -0,0 +1,32 @@
+﻿namespace Heranca1.Entities
+{
+    class Account // saldo
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; } // Saldo
+        // se usarmos o private no set, só poderemos alterar na propria classe
+        // para poder modificar nas classes herdadas vamos usar o protected
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount) // metodo de saque
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount) // metodo de deposito
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Heranca1/Entities/SavingsAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace Heranca1.Entities
+{
+    class SavingsAccount : Account // Conta Poupança
+    {
+        public double InterestRate { get; set; } //tx de juros
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        // atualizaçao do saldo da conta com base na taxa de juros
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
