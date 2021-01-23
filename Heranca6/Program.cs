79ca8a982fbf92cb328837e17bf1f8185445fdc9
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-23 19:23:18 -0300

    Heranca 6 NA S09 aula 134

---
+++ Heranca6/Program.cs
@@ -0,0 +1,54 @@
+﻿using Heranca6.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Heranca6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> list = new List<Account>();
+
+            list.Add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+            list.Add(new BusinessAccount(1002, "Maria", 500.0, 400.0));
+            list.Add(new SavingsAccount(1004, "Bob", 500.0, 0.01));
+            list.Add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
+
+            double sum = 0.0;
+            foreach (Account acc in list)
+            {
+                sum += acc.Balance;
+            }
+
+            Console.WriteLine("Total balance: " + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+            foreach (Account acc in list)
+            {
+                acc.Withdraw(10.0);
+            }
+            foreach (Account acc in list)
+            {
+                Console.WriteLine("Updated balance for account "
+                    + acc.Number
+                    + ": "
+                    + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+/*
+ * CLASSES ABSTRATAS
+ * 
+ * São classes que não podem ser instanciadas
+ * É uma forma de garantir herança total: somente subclasses não
+ * abstratas podem ser instanciadas, mas nunca a superclasse abstrata
+ * 
+ * 
+ * 
+ */
