c887c104b2b00d7911740547684b057948e12250
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-23 22:06:03 -0300

    Excecoes 1 NA S10 aula 143

---
+++ Excecoes1/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Excecoes1.Entities.Exceptions {
+    class DomainException : ApplicationException {
+
+        public DomainException(string message) : base(message) {
+        }
+    }
+}
