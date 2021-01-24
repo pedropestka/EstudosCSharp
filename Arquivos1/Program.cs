e8da287b6a645d5809fa4f23da6474dafd6cefe1
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-23 23:58:26 -0300

    Arquivos 1 FileInfo NA S13 aula 186

---
+++ Arquivos1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // arquivo de origem
+            string soursePath = @"C:\temp\file1.txt";
+            // ou string soursePath = "C:\\temp\\file1.txt";
+
+
+            // arquivo de destino
+            string targetPath = @"C:\temp\file2.txt";
+
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(soursePath);
+                fileInfo.CopyTo(targetPath); // cria o file2
+
+                string[] lines = File.ReadAllLines(soursePath); // cria um vetor com as linhas do arquivo
+
+                foreach (string s in lines)
+                {
+                    Console.WriteLine(s);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurr");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
