0ebbae5f01cb77bf4267e229ddb2e4e4c873a143
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-24 13:23:12 -0300

    Arquivos 2 StreamReader FileStream File OpenText EndOfStream FileMode Close NA S13 aula 186

---
+++ Arquivos2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //acesso a um arquivo -->> é necessário instanciar dois objetos
+
+            string path = @"C:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            
+            try
+            {
+                //instanciar o filestream associado ao file
+                fs = new FileStream(path, FileMode.Open); // abrir file >>> FileMode.Open)
+
+                //instanciar o obj streamreader associado ao filestream
+                sr = new StreamReader(fs);
+
+                //lendo apenas a primeira linha
+                //string line = sr.ReadLine();
+                //Console.WriteLine(line);
+
+                // lendo todas as linhas do arquivo:
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Arquivos2b/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // forma resumida de instanciar o StreamReader
+
+            string path = @"C:\temp\file1.txt";
+            StreamReader sr = null;
+
+            try
+            {
+                //instanciar o obj streamreader utilizando como auxiliar a classe File
+                // o OpenText implicitamente instancia o FileStream, e instancia o StreamReader
+                sr = File.OpenText(path); 
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
