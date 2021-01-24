f12a7a40e236180d63f256efdbb01516a4355e2f
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-24 14:30:12 -0300

    Arquivos 3 Bloco using NA S13 aula 188

---
+++ Arquivos3b/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos3b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+/*
+ * Sem o FileStream
+ */
