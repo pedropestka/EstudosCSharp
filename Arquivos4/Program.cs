37397980598d016a853af4e07f2f8fdc789f03f8
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-24 14:42:44 -0300

    Arquivos 4 StreamWriter AppendText WriteLine ToUpper NA S13 aula 189

---
+++ Arquivos4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string soursePath = @"C:\temp\file1.txt";
+            string targetPath = @"C:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(soursePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string sl in lines)
+                    {
+                        sw.WriteLine(sl.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+/*
+ * AppendText - concatena ao texto exitente - vai acrescentar o texto ao final do arquivo
+ * WriteLine
+ * ToUpper
+ */
