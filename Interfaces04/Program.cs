10f4ff5face96b24a9595f4b0216fc7f13001e00
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-26 19:12:41 -0300

    Interfaces 4 NA S14 aula 201

--- Interfaces04/Program.cs
+++ Interfaces04/Program.cs
@@ -11,6 +11,8 @@ namespace Interfaces04
             IShape s2 = new Rectangle() { Width = 3.5, Height = 4.2, Color = Color.Black };
             Console.WriteLine(s1);
             Console.WriteLine(s2);
+
+            Console.ReadLine();
         }
     }
 }
