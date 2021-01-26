2d97df8f94769633cec408a0798023d6acac0d91
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-26 10:57:25 -0300

    Interfaces 3 NA S14 aula 199

---
+++ Interfaces03/Services/ContractService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Interfaces03.Entities;
+
+namespace Interfaces03.Services {
+    class ContractService {
+
+        private IOnlinePaymentService _onlinePaymentService;
+
+        public ContractService(IOnlinePaymentService onlinePaymentService) {
+            _onlinePaymentService = onlinePaymentService;
+        }
+
+        public void ProcessContract(Contract contract, int months) {
+            double basicQuota = contract.TotalValue / months;
+            for (int i = 1; i <= months; i++) {
+                DateTime date = contract.Date.AddMonths(i);
+                double updatedQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i);
+                double fullQuota =  updatedQuota + _onlinePaymentService.PaymentFee(updatedQuota);
+                contract.AddInstallment(new Installment(date, fullQuota));
+            }
+        }
+    }
+}
---
+++ Interfaces03/Services/IOnlinePaymentService.cs
@@ -0,0 +1,7 @@
+﻿namespace Interfaces03.Services {
+    interface IOnlinePaymentService {
+
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
