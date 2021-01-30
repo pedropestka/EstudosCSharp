8a978ee8d9343a441f7cc99cf6529a4ff55f9d5e
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-30 20:47:45 -0300

    Dicionario 2 NA S15 Aula 214

---
+++ Dicionario02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dicionario02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Dictionary<string, int> votacao = new Dictionary<string, int>();
+
+			Console.Write("Enter file full path: "); //C:\temp\dicionario214.txt
+			string path = Console.ReadLine();
+
+			using (StreamReader sr = File.OpenText(path))
+			{
+				while (!sr.EndOfStream)
+				{
+					string[] linhatxt = sr.ReadLine().Split(',');
+					string name = linhatxt[0];
+					int votos = int.Parse(linhatxt[1]);
+
+					if (votacao.ContainsKey(name))
+					{
+						votacao[name] += votos;
+					}
+					else
+					{
+						votacao[name] = votos;
+					}
+				}
+			}
+			foreach (KeyValuePair<string, int> elemento in votacao)
+			{
+				Console.WriteLine(elemento.Key + ": " + elemento.Value);
+			}
+			Console.ReadLine();
+		}
+	}
+}
