c47c7f2d7c86516744b91ec690cc793b3165aeaf
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-30 17:36:48 -0300

    Set 3 NA S15 Aula 212

---
+++ Set03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Set03
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			HashSet<int> a = new HashSet<int>();
+			HashSet<int> b = new HashSet<int>();
+			HashSet<int> c = new HashSet<int>();
+			HashSet<int> totalAlunos = new HashSet<int>();
+
+			Console.Write("How many students for course A? ");
+			int na = int.Parse(Console.ReadLine());
+			for (int i = 0; i < na; i++)
+			{
+				a.Add(int.Parse(Console.ReadLine()));
+			}
+
+			Console.Write("How many students for course B? ");
+			int nb = int.Parse(Console.ReadLine());
+			for (int i = 0; i < nb; i++)
+			{
+				b.Add(int.Parse(Console.ReadLine()));
+			}
+
+			Console.Write("How many students for course C? ");
+			int nc = int.Parse(Console.ReadLine());
+			for (int i = 0; i < nc; i++)
+			{
+				c.Add(int.Parse(Console.ReadLine()));
+			}
+
+			totalAlunos.UnionWith(a);
+			totalAlunos.UnionWith(b);
+			totalAlunos.UnionWith(c);
+
+			Console.WriteLine("Total students: " + totalAlunos.Count);
+
+
+			Console.ReadLine();
+		}
+	}
+}
