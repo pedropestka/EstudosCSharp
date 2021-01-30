bbc7e82a6b71bac3394e684829f16faf544d05e8
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-30 16:51:20 -0300

    Set 2 HashSet Equals GetHashCode NA S15 Aula 211

---
+++ Set02/Program.cs
@@ -0,0 +1,39 @@
+﻿using Set02.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Set02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+			
+			
+			Console.Write("Enter file full path: "); //‪C:\temp\set210.txt
+			string path = Console.ReadLine();
+			try
+			{
+				using (StreamReader sr = File.OpenText(path))
+				{
+					while (!sr.EndOfStream)
+					{
+						string[] line = sr.ReadLine().Split(' ');
+						string name = line[0];
+						DateTime instant = DateTime.Parse(line[1]);
+						set.Add(new LogRecord { Username = name, Instant = instant });
+					}
+					Console.WriteLine("Total Users: " + set.Count);
+				}
+			}
+			catch (IOException e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			Console.ReadLine();
+		}
+	}
+}
