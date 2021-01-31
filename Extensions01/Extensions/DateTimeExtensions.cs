b61148a20be5c4ce6e84fef8b35c5ea878d172b6
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-01-30 21:48:16 -0300

    Extensions 1 NA S15 Aula 217

---
+++ Extensions01/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace System
+{
+	static class DateTimeExtensions
+	{
+		public static string ElapsedTime(this DateTime thisObj)
+		{
+			TimeSpan duracao = DateTime.Now.Subtract(thisObj);
+			if (duracao.TotalHours < 24)
+			{
+				return duracao.TotalHours.ToString("F1", CultureInfo.InvariantCulture);
+			}
+			else
+			{
+				return duracao.TotalDays.ToString("F1", CultureInfo.InvariantCulture);
+			}
+		}
+	}
+}
---
+++ Extensions01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Extensions01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DateTime dt = new DateTime(2021, 01, 30, 16, 42, 10);
+			Console.WriteLine(dt.ElapsedTime());
+			Console.ReadLine();
+		}
+	}
+}
+
+/*
+Vamos criar um extension method
+chamado "ElapsedTime()" no struct
+DateTime para apresentar um objeto DateTime na forma de tempo
+decorrido, podendo ser em horas (se menor que 24h) ou em dias caso
+contrário. Por exemplo:
+DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+Console.WriteLine(dt.ElapsedTime());
+"4.5 hours"
+"3.2 days"
+*/
